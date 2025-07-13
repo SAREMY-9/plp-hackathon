ff9b31375854961560b89e6317cb0c4bb4ed358f
SAREMY-9 <brianowino226@gmail.com>
2025-07-14 00:19:21 +0300

    reactivated links

--- script.js
+++ script.js
@@ -52,3 +52,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+  const headingText = "Where Strategy Meets Seamless Code";
+  const subText = "";
+
+  const headingElement = document.getElementById("headingText");
+  const subTextElement = document.getElementById("subText");
+
+  let i = 0, j = 0;
+
+  function typeHeading() {
+    if (i < headingText.length) {
+      headingElement.innerHTML += headingText.charAt(i);
+      i++;
+      setTimeout(typeHeading, 50); // typing speed (ms)
+    } else {
+      setTimeout(typeSubtext, 300); // delay before subtext starts
+    }
+  }
+
+  function typeSubtext() {
+    if (j < subText.length) {
+      subTextElement.innerHTML += subText.charAt(j);
+      j++;
+      setTimeout(typeSubtext, 30);
+    }
+  }
+
+  // Start typing when page loads
+  window.onload = typeHeading;
